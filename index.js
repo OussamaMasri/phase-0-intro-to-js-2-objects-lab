85eb9df351b49e869d1402f04e9b6c591c2ba5f7
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-08 23:55:43 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {name:"Oussama",
+streetAddress:"Tripoli"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newstate = { ...employee };
+    newstate[key] = value;
+    return newstate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) { 
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey (employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
